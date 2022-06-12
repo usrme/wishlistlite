b18651dd68b09cfe3112c8b7d56fd8e9a654e906
Üllar Seerme <yates@protonmail.ch>
2022-06-12 15:26:43 +0300

    test(wishlistlite_test.go): make sure received hosts match expected not only in length of slice

--- wishlistlite_test.go
+++ wishlistlite_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"testing"
+
+	"github.com/charmbracelet/bubbles/list"
 )
 
 func TestSshConfigHosts(t *testing.T) {
@@ -28,4 +30,30 @@ func TestSshConfigHosts(t *testing.T) {
 			}
 		})
 	}
+	t.Run("expected hosts", func(t *testing.T) {
+		expected := []list.Item{
+			Item{Host: "darkstar", Hostname: "darkstar.local"},
+			Item{Host: "supernova", Hostname: "supernova.local"},
+			Item{Host: "app1", Hostname: "app.foo.local"},
+			Item{Host: "app2", Hostname: "app.foo.local"},
+			Item{Host: "multiple1", Hostname: "multi1.foo.local"},
+			Item{Host: "multiple2", Hostname: "multi2.foo.local"},
+			Item{Host: "multiple3", Hostname: "multi3.foo.local"},
+			Item{Host: "no.hostname", Hostname: "no.hostname"},
+			Item{Host: "req.tty", Hostname: "req.tty"},
+			Item{Host: "remote.cmd", Hostname: "remote.cmd"},
+			Item{Host: "only.host", Hostname: "only.host"},
+		}
+		hosts, err := sshConfigHosts("testdata/good")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for i := range hosts {
+			if hosts[i] != expected[i] {
+				t.Errorf("got %s, wanted %d", hosts[i], expected[i])
+			}
+		}
+	})
 }
